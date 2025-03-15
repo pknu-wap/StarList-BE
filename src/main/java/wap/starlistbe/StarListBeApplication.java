cc43468cb75efd20d11058430dba67635e005a66
gyunho <gyunh01@pukyong.ac.kr>
2025-03-16 00:01:47 +0900

    first commit

---
+++ src/main/java/wap/starlistbe/StarListBeApplication.java
@@ -0,0 +1,13 @@
+package wap.starlistbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarListBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarListBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wap/starlistbe/StarListBeApplicationTests.java
@@ -0,0 +1,13 @@
+package wap.starlistbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarListBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
